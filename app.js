b118637d25cc8b385558daf0f96f24728cd5fa70
Kevin Martinez <n4utilius@gmail.com>
2012-06-10 02:26:22 -0500

    parseo a infografia dinamica

---
+++ app.js
@@ -0,0 +1,156 @@
+/** Module dependencies. */
+  var express = require('express'), 
+      jsdom = require('jsdom'),
+      fs = require('fs'), 
+      request = require('request'), 
+      url = require('url'), 
+      app = module.exports = express.createServer(),
+      routes = require('./routes');
+
+// Configuration
+
+  app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+  });
+
+  app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
+
+  app.configure('production', function(){
+    app.use(express.errorHandler());
+  });
+
+// Routes
+
+  app.get('/', routes.index);
+
+  app.get('/reporte/:estado', function(req, res){
+    var uri = 'http://publicidadoficial.com.mx/' + req.params.estado;
+    request({uri: uri}, 
+      function(err, response, body){
+        var self = this;
+        self.items = new Array();
+   
+        if(err && response.statusCode !== 200){console.log('Request error.');}
+        jsdom.env(
+          { html: body, scripts: ['http://code.jquery.com/jquery-1.6.min.js'] }, 
+          function(err, window){
+            var $ = window.jQuery;
+
+            var data = {};
+              data.img_estrellas = $('div#estrellas').attr('style').split("url('")[1].split("')")[0];
+
+              gasto_temp = $('#gastoPerCapita p span img').attr('src');
+              data.gasto_percapita = (gasto_temp)? "¿?" : $('#gastoPerCapita #bigMoney').text();
+  
+              data.gasto_anios = [];
+              objeto = { 'concepto':'', '2010':'2010', '2011':'2011'};
+              data.gasto_anios.push(objeto);
+
+              $('#tablaGasto .tableRow').each(function(){
+                  concepto = $(this).children('.tableLabel').text();
+                  val_2010 = $(this).children('.tableValue:first').text();
+                  val_2011 = $(this).children('.tableValue:last').text();
+  
+                  objeto = { 'concepto':concepto, '2010': val_2010, '2011': val_2011};
+                  data.gasto_anios.push(objeto);
+              });
+
+              data.cronologia = [];
+              $('.entry-content span, .entry-content em').each(function(){
+                if($(this).text() != ""){
+                  item = {"texto": $(this).text(), "tipo": this.nodeName.toLowerCase()};
+                  data.cronologia.push(item);
+                }
+              });
+
+              data.fechas = []; data.gastos = [];
+              $('#tablaMoney tr:first td').each(function(){ data.fechas.push( $(this).text() );  });
+              $('#tablaMoney tr:last td').each(function(){  data.gastos.push( $(this).text() );  });
+              
+              data.archivo_tipo_medio = $('div#archivoTipoMedio').text(),
+
+              data.pleca_verde = $('div.plecaVerde').text(), 
+
+              data.enlaces = [];
+              $('#wrapLinks p').each(function(){  data.enlaces.push( $(this).text() ); });
+
+              data.gasto_intenet_texto = $('div#gastoIntenetTexto').text();
+            
+            function parse_html(data){
+
+              // [{ 'concepto':'', '2010':'', '2011':''}, { 'concepto':'', '2010':'', '2011':''}]
+              var filas_gastos = "";
+              for (d in data.gasto_anios){
+                  ga = data.gasto_anios[d];
+                  filas_gastos += "<tr>";
+                  filas_gastos +=   "<td>" + ga['concepto'] + "<td>";
+                  filas_gastos +=   "<td>" + ga['2010'] + "<td>";
+                  filas_gastos +=   "<td>" + ga['2011'] + "<td>";
+                  filas_gastos += "</tr>";
+              }
+
+              // [{ 'concepto':'', '2011':''}, { 'concepto':'', '2011':''}]
+              var cronologia = "<ul>";
+              for (c in data.cronologia){
+                  cron = data.cronologia[c];
+
+                  if(cron.tipo == 'em'){
+                    cronologia += "<em>" + cron.texto + "</em>";
+                  }else{
+                    cronologia += "<li>" + cron.texto + "</li>";
+                  }
+              }
+              cronologia += "</ul>";
+
+              var html  = "<div id='balance'>";
+                  html +=   "<div id ='resumen'>";
+                  html +=     "<div>";
+                  html +=       "<h2>Transparencia y acceso a la Información</h2>";
+                  html +=       "<img id='calificacion' src='" + data.img_estrellas + "' />";
+                  html +=     "</div>";
+                  html +=     "<div>";
+                  html +=       "<h2>Gasto en comunicación 2010 per capita</h2>";
+                  html +=       "<div id='fondo_gasto'>";
+                  html +=         "<label id='gasto'>" + data.gasto_percapita + "</label>";
+                  html +=       "</div>";
+                  html +=     "</div>";
+                  html +=   "</div>";
+                  html +=   "<div id ='reporte_datos'>";
+                  html +=     "<h2>Información Recibida</h2>";
+                  html +=     "<table id='tabla_gastos'>" + filas_gastos + "</table>";
+                  html +=   "</div>";
+                  html += "</div>";
+                  html += "<div id='cronologia'>";
+                  html +=   "<h2>Acceso al gasto en publicidad oficial vía solicitudes de información</h2>";
+                  html +=    cronologia;
+                  html += "</div>";
+
+              return html;
+            }
+
+            //"http://publicidadoficial.com.mx/wp-content/themes/publicidadoficial/images/
+            //http://publicidadoficial.com.mx/wp-content/themes/publicidadoficial/images/0estrellas.jpg
+
+            var data_html = parse_html(data);
+            res.end(data_html);
+          }
+        );
+      }
+    );
+  });
+
+  app.listen(3000, function(){
+    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  });
+
+
+
+            
---
+++ public/javascripts/init.js
@@ -0,0 +1,95 @@
+$(function(){
+	
+	var r = Raphael('map', 900, 615),
+		attributes = {
+            fill: '#fff',
+            stroke: '#3899E6',
+            'stroke-width': 1,
+            'stroke-linejoin': 'round'
+        },
+		arr = new Array();
+	
+	//setTimeout(function(){$("#info").fadeIn(2000)}, 500);
+
+	for (var state in paths) {
+		var obj = r.path(paths[state].path);
+		obj.attr(attributes);
+		arr[obj.id] = state;
+
+		base_url = "http://localhost:3000/reporte/";
+
+		var fijar_estado = false;		
+		obj
+			.hover(
+				function(){ 
+					state_name = paths[arr[this.id]].name
+					image_url  = "http://publicidadoficial.com.mx/wp-content/themes/publicidadoficial/images/";
+					image_url += normalize(state_name) + ".png";
+
+					this.animate({ fill: '#1669AD' }, 300); 					
+					this.animate({ fill: '#1669AD' }, 300); 
+					
+					$("#title_bar label#name_estado")
+						.css("display","none")
+						.html(state_name + "<img id='img_estado' src='" + image_url + "'/>");
+
+					$("#title_bar label#name_estado").fadeIn("slow");
+				}, 
+				function(){ 
+					this.animate({ fill: attributes.fill }, 300); 
+					if(fijar_estado === false){
+						$("#info label#name_estado").html("<small>Da click y accede a la información</small>");
+					}
+				}
+			)
+			
+			.click(function(){
+				fijar_estado = false;
+				state_name = paths[arr[this.id]].name
+
+				$("#protector_map").css("display", "inline");
+				$('html, body').animate({scrollTop:570}, 'slow');
+
+				url = "http://localhost:3000/reporte/" + state_name;
+				$.get(url, {}, function(data){
+					$("div#status").html(data);
+					$("div#status").slideDown(700);
+				})
+				fijar_estado = true;
+				return false;
+			});
+
+		var normalize = (function() {
+		  var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç",
+		      to   = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",
+		      mapping = {};
+		 
+		  for(var i = 0, j = from.length; i < j; i++ )
+		      mapping[ from.charAt( i ) ] = to.charAt( i );
+		 
+		  return function( str ) {
+		      var ret = [];
+		      for( var i = 0, j = str.length; i < j; i++ ) {
+		          var c = str.charAt( i );
+		          if( mapping.hasOwnProperty( str.charAt( i ) ) )
+		              ret.push( mapping[ c ] );
+		          else
+		              ret.push( c );
+		      }
+		      return ret.join( '' ).replace( /[^-A-Za-z0-9]+/g, '-' ).toLowerCase();
+		  }		 
+		})();
+	}
+
+	$(window).scroll(function(){
+		scroll_top = $(this).scrollTop();
+		if(scroll_top <= 43){
+			$("#protector_map").css("display","none");
+			$("#status").slideUp("fast", function(){
+				$(this).css("display","none");
+				//$("#title_bar").html("<small>Pasa el cursor por los estados...</small>");
+			})
+		}
+	})
+});
+
